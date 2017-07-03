4660a9403cf0545aa8944b7d1b2b33b26eb5f503
abicelis <abicelis@gmail.com>
2017-07-03 19:34:29 -0400

    Calendar and File util implementation

---
+++ app/src/main/java/ve/com/abicelis/chefbuddy/util/CalendarUtil.java
@@ -0,0 +1,29 @@
+package ve.com.abicelis.chefbuddy.util;
+
+import android.support.annotation.Nullable;
+
+import java.util.Calendar;
+/**
+ * Created by abice on 26/4/2017.
+ */
+
+public class CalendarUtil {
+
+    public static Calendar getNewInstanceZeroedCalendar() {
+        Calendar cal = Calendar.getInstance();
+        cal.set(Calendar.HOUR_OF_DAY, 0);
+        cal.set(Calendar.MINUTE, 0);
+        cal.set(Calendar.SECOND, 0);
+        cal.set(Calendar.MILLISECOND, 0);
+        return cal;
+    }
+
+    public static void copyCalendar(Calendar copyFrom, @Nullable Calendar copyTo) {
+        if(copyFrom == null)
+            throw new NullPointerException("copyCalendar(), copyFrom parameter is null");
+
+        copyTo.setTimeZone(copyFrom.getTimeZone());
+        copyTo.setTimeInMillis(copyFrom.getTimeInMillis());
+    }
+
+}
---
+++ app/src/main/java/ve/com/abicelis/chefbuddy/util/FileUtil.java
@@ -0,0 +1,94 @@
+package ve.com.abicelis.chefbuddy.util;
+
+import android.app.Activity;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.channels.FileChannel;
+
+import ve.com.abicelis.chefbuddy.R;
+
+/**
+ * Created by abicelis on 3/7/2017.
+ */
+
+public class FileUtil {
+    /**
+     * Creates the specified <code>toFile</code> as a byte for byte copy of the
+     * <code>fromFile</code>. If <code>toFile</code> already exists, then it
+     * will be replaced with a copy of <code>fromFile</code>. The name and path
+     * of <code>toFile</code> will be that of <code>toFile</code>.<br/>
+     * <br/>
+     * <i> Note: <code>fromFile</code> and <code>toFile</code> will be closed by
+     * this function.</i>
+     *
+     * @param fromFile
+     *            - FileInputStream for the file to copy from.
+     * @param toFile
+     *            - FileInputStream for the file to copy to.
+     */
+    public static void copyFile(FileInputStream fromFile, FileOutputStream toFile) throws IOException {
+        FileChannel fromChannel = null;
+        FileChannel toChannel = null;
+        try {
+            fromChannel = fromFile.getChannel();
+            toChannel = toFile.getChannel();
+            fromChannel.transferTo(0, fromChannel.size(), toChannel);
+        } finally {
+            try {
+                if (fromChannel != null) {
+                    fromChannel.close();
+                }
+            } finally {
+                if (toChannel != null) {
+                    toChannel.close();
+                }
+            }
+        }
+    }
+
+    public static File getImageAttachmentDir(Activity activity) {
+        return new File(activity.getExternalFilesDir(null), activity.getResources().getString(R.string.subdirectory_attachments_image));
+    }
+
+    public static void createDirIfNotExists(File directory) throws IOException, SecurityException  {
+        if (directory.mkdirs()){
+            File nomedia = new File(directory, ".nomedia");
+            nomedia.createNewFile();
+        }
+    }
+
+
+    /**
+     * Creates an empty file at the specified directory, with the given name if it doesn't already exist
+     *
+     */
+    public static File createNewFileIfNotExistsInDir(File directory, String fileName) throws IOException {
+        File file = new File(directory, fileName);
+        file.createNewFile();
+        return file;
+    }
+
+
+
+    public static void deleteImageAttachment(Activity activity, String filename) {
+        if(filename != null && !filename.isEmpty()) { //Delete file
+            File file = new File(FileUtil.getImageAttachmentDir(activity), filename);
+            file.delete();
+        }
+    }
+
+
+//    public static File createTempImageFileInDir(File directory, String fileExtension) throws IOException, SecurityException {
+//        if(fileExtension.toCharArray()[0] != '.')
+//            fileExtension = "." + fileExtension;
+//
+//        //String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+//        String fileName = "TEMP_" + UUID.randomUUID().toString() + "_";
+//        File file = File.createTempFile(fileName, fileExtension, directory);
+//        return file;
+//    }
+
+}
