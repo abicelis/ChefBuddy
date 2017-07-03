d4d55b9c8992cc3ca2ad84c726dd74a9102385dd
abicelis <abicelis@gmail.com>
2017-07-03 19:35:43 -0400

    Built ChefBuddy sql contract

---
+++ app/src/main/java/ve/com/abicelis/chefbuddy/database/ChefBuddyContract.java
@@ -0,0 +1,36 @@
+package ve.com.abicelis.chefbuddy.database;
+
+import android.provider.BaseColumns;
+
+/**
+ * Created by abicelis on 3/7/2017.
+ */
+final class ChefBuddyContract {
+
+    // To prevent someone from accidentally instantiating the contract class,
+    // give it an empty constructor.
+    ChefBuddyContract() { }
+
+    /* Recipe Table  */
+    static abstract class RecipeTable implements BaseColumns {
+        static final String TABLE_NAME = "recipe";
+
+        static final TableColumn COL_NAME_NAME = new TableColumn(DataType.TEXT, "name");
+        static final TableColumn COL_NAME_SERVINGS = new TableColumn(DataType.INTEGER, "servings");
+        static final TableColumn COL_NAME_PREPARATION_TIME = new TableColumn(DataType.TEXT, "preparation_time");
+        static final TableColumn COL_NAME_PREPARATION_TIME_TYPE = new TableColumn(DataType.TEXT, "preparation_time_type");
+        static final TableColumn COL_NAME_DIRECTIONS = new TableColumn(DataType.TEXT, "directions");
+        static final TableColumn COL_NAME_FEATURED_IMAGE = new TableColumn(DataType.BLOB, "featured_image");
+    }
+
+    /* Daily Recipe Table */
+    static abstract class DailyRecipeTable implements BaseColumns {
+        static final String TABLE_NAME = "daily_recipe";
+
+        static final TableColumn COL_NAME_YEAR = new TableColumn(DataType.INTEGER, "year");
+        static final TableColumn COL_NAME_MONTH = new TableColumn(DataType.INTEGER, "month");
+        static final TableColumn COL_NAME_DAY = new TableColumn(DataType.INTEGER, "date");
+        static final TableColumn COL_NAME_RECIPE_FK = new TableColumn(DataType.INTEGER, "fk_recipe");
+    }
+
+}
