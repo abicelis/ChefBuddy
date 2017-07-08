4b133e66a4f21213bcb23acefbc51b15d815a9e4
abicelis <abicelis@gmail.com>
2017-07-08 13:31:27 -0400

    Misc

--- app/src/main/java/ve/com/abicelis/chefbuddy/ui/main/MainActivity.java
+++ app/src/main/java/ve/com/abicelis/chefbuddy/ui/main/MainActivity.java
@@ -4,19 +4,38 @@ import android.graphics.drawable.Drawable;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.lukedeighton.wheelview.WheelView;
 import com.lukedeighton.wheelview.adapter.WheelAdapter;
 
+import javax.inject.Inject;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 import ve.com.abicelis.chefbuddy.R;
+import ve.com.abicelis.chefbuddy.app.ChefBuddyApplication;
+import ve.com.abicelis.chefbuddy.database.ChefBuddyDAO;
+import ve.com.abicelis.chefbuddy.database.exceptions.CouldNotGetDataException;
+import ve.com.abicelis.chefbuddy.model.Recipe;
 
 public class MainActivity extends AppCompatActivity {
 
+    //DATA
+    @Inject
+    ChefBuddyDAO mDao;
+    private static final String TAG = MainActivity.class.getSimpleName();
 
+    //UI
+    @BindView(R.id.label)
     TextView label;
+
+    @BindView(R.id.label2)
     TextView label2;
+
+    @BindView(R.id.wheelview)
     WheelView wheelView;
 
     @Override
@@ -24,9 +43,17 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        wheelView = (WheelView) findViewById(R.id.wheelview);
-        label = (TextView) findViewById(R.id.label);
-        label2 = (TextView) findViewById(R.id.label2);
+        ((ChefBuddyApplication)getApplication()).getAppComponent().inject(this);
+        ButterKnife.bind(this);
+
+        try {
+            for (Recipe recipe : mDao.getRecipes()) {
+                Log.d(TAG, "Found recipe in DB:\n\r" + recipe.toString() + "\n\r\n\r");
+            }
+        } catch (CouldNotGetDataException e) {
+            Log.d(TAG, "Error getting recipes from DB");
+        }
+
 
         wheelView.setAdapter(new WheelAdapter() {
             @Override
