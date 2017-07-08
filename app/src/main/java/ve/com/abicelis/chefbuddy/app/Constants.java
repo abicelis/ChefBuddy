aa8e0a1305968b5f4c56ea535c0e586acd448372
abicelis <abicelis@gmail.com>
2017-07-08 14:52:38 -0400

    Constants class for app global constants

---
+++ app/src/main/java/ve/com/abicelis/chefbuddy/app/Constants.java
@@ -0,0 +1,12 @@
+package ve.com.abicelis.chefbuddy.app;
+
+
+/**
+ * Created by abicelis on 8/7/2017.
+ */
+
+public class Constants {
+
+  public static final String API_KEY = "394TsayTE0ZxEgLLwuCvpfQWyoWTjGHF3UivkfZB";
+
+}
