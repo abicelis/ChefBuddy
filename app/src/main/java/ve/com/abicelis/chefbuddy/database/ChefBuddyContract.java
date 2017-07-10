be4bdbe0d72f27418b50b6925d204608c6e5e96d
abicelis <abicelis@gmail.com>
2017-07-10 16:19:51 -0400

    Removed BaseColumns interface, using own ID fields for SQL tables

--- app/src/main/java/ve/com/abicelis/chefbuddy/database/ChefBuddyContract.java
+++ app/src/main/java/ve/com/abicelis/chefbuddy/database/ChefBuddyContract.java
@@ -11,42 +11,46 @@ final class ChefBuddyContract {
     // give it an empty constructor.
     ChefBuddyContract() { }
 
-    /* Ingredient Table  */
-    static abstract class IngredientTable implements BaseColumns {
+    /* RecipeIngredient Table  */
+    static abstract class IngredientTable {
         static final String TABLE_NAME = "ingredient";
-        static final TableColumn COL_NAME_NAME = new TableColumn(DataType.TEXT, "name");
+        static final TableColumn COLUMN_ID = new TableColumn(DataType.INTEGER, "ingredient_id");
+        static final TableColumn COLUMN_NAME = new TableColumn(DataType.TEXT, "name");
     }
 
     /* Recipe Table  */
-    static abstract class RecipeTable implements BaseColumns {
+    static abstract class RecipeTable {
         static final String TABLE_NAME = "recipe";
 
-        static final TableColumn COL_NAME_NAME = new TableColumn(DataType.TEXT, "name");
-        static final TableColumn COL_NAME_SERVINGS = new TableColumn(DataType.INTEGER, "servings");
-        static final TableColumn COL_NAME_PREPARATION_TIME = new TableColumn(DataType.TEXT, "preparation_time");
-        static final TableColumn COL_NAME_PREPARATION_TIME_TYPE = new TableColumn(DataType.TEXT, "preparation_time_type");
-        static final TableColumn COL_NAME_DIRECTIONS = new TableColumn(DataType.TEXT, "directions");
-        static final TableColumn COL_NAME_FEATURED_IMAGE = new TableColumn(DataType.BLOB, "featured_image");
+        static final TableColumn COLUMN_ID = new TableColumn(DataType.INTEGER, "recipe_id");
+        static final TableColumn COLUMN_NAME = new TableColumn(DataType.TEXT, "name");
+        static final TableColumn COLUMN_SERVINGS = new TableColumn(DataType.INTEGER, "servings");
+        static final TableColumn COLUMN_PREPARATION_TIME = new TableColumn(DataType.TEXT, "preparation_time");
+        static final TableColumn COLUMN_PREPARATION_TIME_TYPE = new TableColumn(DataType.TEXT, "preparation_time_type");
+        static final TableColumn COLUMN_DIRECTIONS = new TableColumn(DataType.TEXT, "directions");
+        static final TableColumn COLUMN_FEATURED_IMAGE = new TableColumn(DataType.BLOB, "featured_image");
     }
 
     /* RecipeIngredient Table  */
-    static abstract class RecipeIngredientTable implements BaseColumns {
+    static abstract class RecipeIngredientTable {
         static final String TABLE_NAME = "recipe_ingredient";
 
-        static final TableColumn COL_NAME_RECIPE_FK = new TableColumn(DataType.TEXT, "fk_recipe");
-        static final TableColumn COL_NAME_INGREDIENT_FK = new TableColumn(DataType.TEXT, "fk_ingredient");
-        static final TableColumn COL_NAME_AMOUNT = new TableColumn(DataType.TEXT, "amount");
-        static final TableColumn COL_NAME_MEASUREMENT = new TableColumn(DataType.TEXT, "measurement");
+        static final TableColumn COLUMN_ID = new TableColumn(DataType.INTEGER, "recipe_ingredient_id");
+        static final TableColumn COLUMN_RECIPE_FK = new TableColumn(DataType.TEXT, "fk_recipe");
+        static final TableColumn COLUMN_INGREDIENT_FK = new TableColumn(DataType.TEXT, "fk_ingredient");
+        static final TableColumn COLUMN_AMOUNT = new TableColumn(DataType.TEXT, "amount");
+        static final TableColumn COLUMN_MEASUREMENT = new TableColumn(DataType.TEXT, "measurement");
     }
 
     /* Daily Recipe Table */
-    static abstract class DailyRecipeTable implements BaseColumns {
+    static abstract class DailyRecipeTable {
         static final String TABLE_NAME = "daily_recipe";
 
-        static final TableColumn COL_NAME_YEAR = new TableColumn(DataType.INTEGER, "year");
-        static final TableColumn COL_NAME_MONTH = new TableColumn(DataType.INTEGER, "month");
-        static final TableColumn COL_NAME_DAY = new TableColumn(DataType.INTEGER, "date");
-        static final TableColumn COL_NAME_RECIPE_FK = new TableColumn(DataType.INTEGER, "fk_recipe");
+        static final TableColumn COLUMN_ID = new TableColumn(DataType.INTEGER, "daily_recipe_id");
+        static final TableColumn COLUMN_YEAR = new TableColumn(DataType.INTEGER, "year");
+        static final TableColumn COLUMN_MONTH = new TableColumn(DataType.INTEGER, "month");
+        static final TableColumn COLUMN_DAY = new TableColumn(DataType.INTEGER, "date");
+        static final TableColumn COLUMN_RECIPE_FK = new TableColumn(DataType.INTEGER, "fk_recipe");
     }
 
 }
