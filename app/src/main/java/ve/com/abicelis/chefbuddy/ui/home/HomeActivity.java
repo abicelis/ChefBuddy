f20cef86fe6f313a6077e2e0a1257b16e34663ca
abicelis <abicelis@gmail.com>
2017-07-11 22:32:41 -0400

    MaterialSearchView implementation in home activity

--- app/src/main/java/ve/com/abicelis/chefbuddy/ui/home/HomeActivity.java
+++ app/src/main/java/ve/com/abicelis/chefbuddy/ui/home/HomeActivity.java
@@ -2,14 +2,19 @@ package ve.com.abicelis.chefbuddy.ui.home;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.design.widget.AppBarLayout;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Toast;
+
+import com.miguelcatalan.materialsearchview.MaterialSearchView;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -46,6 +51,12 @@ public class HomeActivity extends AppCompatActivity implements HomeView {
     @BindView(R.id.activity_home_tab_layout)
     TabLayout mTabLayout;
 
+    @BindView(R.id.activity_home_search_view)
+    MaterialSearchView mSearchView;
+
+    @BindView(R.id.activity_home_appbar)
+    AppBarLayout mAppBarLayout;
+
     private HomeViewPagerAdapter mHomeViewPagerAdapter;
     private RecipeListFragment mRecipeListFragment;
     private SpinWheelFragment mSpinWheelFragment;
@@ -72,37 +83,7 @@ public class HomeActivity extends AppCompatActivity implements HomeView {
         getSupportActionBar().setLogo(R.drawable.ic_toolbar_home);
 
         setupViewPagerAndTabLayout();
-
-    }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        presenter.detachView();
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu_home, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        int id = item.getItemId();
-        switch (id) {
-            case R.id.menu_home_about:
-                Toast.makeText(this, "About screen under construction", Toast.LENGTH_SHORT).show();
-                break;
-            case R.id.menu_home_settings:
-                Toast.makeText(this, "Settings screen under construction", Toast.LENGTH_SHORT).show();
-                break;
-            case R.id.menu_home_search:
-                Toast.makeText(this, "Search under construction", Toast.LENGTH_SHORT).show();
-                break;
-        }
-
-        return super.onOptionsItemSelected(item);
+        setupSearchView();
     }
 
     private void setupViewPagerAndTabLayout() {
@@ -132,20 +113,15 @@ public class HomeActivity extends AppCompatActivity implements HomeView {
         mViewpager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
             @Override
             public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
-                // if(mHomeViewPagerAdapter.getRegisteredFragment(position).mActionMode != null)
-                //      mHomeViewPagerAdapter.getRegisteredFragment(position).mActionMode.finish(); //If changing tabs, hide actionMode (ContextMenu)
+
+                //If scrolling out of page one, close search
+                if(position == 0 && mSearchView.isSearchOpen()) {
+                    mSearchView.closeSearch();
+                }
             }
 
             @Override
-            public void onPageSelected(int position) {
-                //If changing tabs, hide actionMode (ContextMenu)
-//                for(int i = 0; i < mHomeViewPagerAdapter.getRegisteredFragments().size(); i++) {
-//                    int key = mHomeViewPagerAdapter.getRegisteredFragments().keyAt(i);
-//
-//                    if(mHomeViewPagerAdapter.getRegisteredFragments().get(key).mActionMode != null)
-//                        mHomeViewPagerAdapter.getRegisteredFragments().get(key).mActionMode.finish();
-//                }
-            }
+            public void onPageSelected(int position) {}
 
             @Override
             public void onPageScrollStateChanged(int state) {}
@@ -153,6 +129,100 @@ public class HomeActivity extends AppCompatActivity implements HomeView {
         mTabLayout.setupWithViewPager(mViewpager);
     }
 
+    private void setupSearchView() {
+        mSearchView.setVoiceSearch(true);
+        mSearchView.setOnQueryTextListener(new MaterialSearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                Log.d(TAG, "onQueryTextSubmit " + query);
+                if(mHomeViewPagerAdapter.getRegisteredFragment(0) != null) {
+                    if (query.isEmpty())
+                        ((RecipeListFragment) mHomeViewPagerAdapter.getRegisteredFragment(0)).cancelFilterRecipes();
+                    else
+                        ((RecipeListFragment) mHomeViewPagerAdapter.getRegisteredFragment(0)).filterRecipes(query);
+                }
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                if(mHomeViewPagerAdapter.getRegisteredFragment(0) != null) {
+                    if (newText.isEmpty())
+                        ((RecipeListFragment) mHomeViewPagerAdapter.getRegisteredFragment(0)).cancelFilterRecipes();
+                    else
+                        ((RecipeListFragment) mHomeViewPagerAdapter.getRegisteredFragment(0)).filterRecipes(newText);
+                }
+                return false;
+            }
+        });
+
+        mSearchView.setOnSearchViewListener(new MaterialSearchView.SearchViewListener() {
+            @Override
+            public void onSearchViewShown() {
+                //mAppBarLayout.setExpanded(false, true);
+                mTabLayout.setVisibility(View.GONE);
+                Log.d(TAG, "onSearchViewShown");
+            }
+
+            @Override
+            public void onSearchViewClosed() {
+                //mAppBarLayout.setExpanded(true, true);
+                mTabLayout.setVisibility(View.VISIBLE);
+                ((RecipeListFragment) mHomeViewPagerAdapter.getRegisteredFragment(0)).cancelFilterRecipes();
+                Log.d(TAG, "onSearchViewClosed");
+            }
+        });
+    }
+
+
+//    @Override
+//    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+//        if (requestCode == MaterialSearchView.REQUEST_VOICE && resultCode == RESULT_OK) {
+//            ArrayList<String> matches = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+//            if (matches != null && matches.size() > 0) {
+//                String searchWrd = matches.get(0);
+//                if (!TextUtils.isEmpty(searchWrd)) {
+//                    mSearchView.setQuery(searchWrd, false);
+//                }
+//            }
+//
+//            return;
+//        }
+//        super.onActivityResult(requestCode, resultCode, data);
+//    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        presenter.detachView();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_home, menu);
+
+        MenuItem item = menu.findItem(R.id.menu_home_search);
+        mSearchView.setMenuItem(item);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        switch (id) {
+            case R.id.menu_home_about:
+                Toast.makeText(this, "About screen under construction", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.menu_home_settings:
+                Toast.makeText(this, "Settings screen under construction", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.menu_home_search:
+                Toast.makeText(this, "Search under construction", Toast.LENGTH_SHORT).show();
+                break;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
 
 
 }
--- app/src/main/java/ve/com/abicelis/chefbuddy/ui/home/HomeViewPagerAdapter.java
+++ app/src/main/java/ve/com/abicelis/chefbuddy/ui/home/HomeViewPagerAdapter.java
@@ -3,6 +3,8 @@ package ve.com.abicelis.chefbuddy.ui.home;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
+import android.util.SparseArray;
+import android.view.ViewGroup;
 
 import java.util.List;
 
@@ -16,7 +18,7 @@ public class HomeViewPagerAdapter extends FragmentPagerAdapter {
     //DATA
     private List<String> mTitleList;
     private List<Fragment> mFragmentList;                                                   //Holds all of the fragments
-    //private SparseArray<HomeListFragment> mRegisteredFragmentList = new SparseArray<>();    //Holds only registered fragments
+    private SparseArray<Fragment> mRegisteredFragmentList = new SparseArray<>();            //Holds only registered fragments
 
 
     public HomeViewPagerAdapter(FragmentManager fm, List<String> titleList, List<Fragment> fragmentList) {
@@ -43,26 +45,26 @@ public class HomeViewPagerAdapter extends FragmentPagerAdapter {
 
 
 
-//    /* These methods are for maintaining and getting registered fragments */
-//
-//    public HomeListFragment getRegisteredFragment(int position) {
-//        return mRegisteredFragmentList.get(position);
-//    }
-//
-//    public SparseArray<HomeListFragment> getRegisteredFragments() {
-//        return mRegisteredFragmentList;
-//    }
-//
-//    @Override
-//    public Object instantiateItem(ViewGroup container, int position) {
-//        Fragment fragment = (Fragment) super.instantiateItem(container, position);
-//        mRegisteredFragmentList.put(position, (HomeListFragment) fragment);
-//        return fragment;
-//    }
-//
-//    @Override
-//    public void destroyItem(ViewGroup container, int position, Object object) {
-//        mRegisteredFragmentList.remove(position);
-//        super.destroyItem(container, position, object);
-//    }
+    /* These methods are for maintaining and getting registered fragments */
+
+    public Fragment getRegisteredFragment(int position) {
+        return mRegisteredFragmentList.get(position);
+    }
+
+    public SparseArray<Fragment> getRegisteredFragments() {
+        return mRegisteredFragmentList;
+    }
+
+    @Override
+    public Object instantiateItem(ViewGroup container, int position) {
+        Fragment fragment = (Fragment) super.instantiateItem(container, position);
+        mRegisteredFragmentList.put(position, fragment);
+        return fragment;
+    }
+
+    @Override
+    public void destroyItem(ViewGroup container, int position, Object object) {
+        mRegisteredFragmentList.remove(position);
+        super.destroyItem(container, position, object);
+    }
 }
