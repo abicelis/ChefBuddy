d343b0a1846d2cc8c9c4e675e5814fed8c5114ec
abicelis <abicelis@gmail.com>
2017-07-13 22:52:37 -0400

    Misc

--- app/src/main/java/ve/com/abicelis/chefbuddy/app/Constants.java
+++ app/src/main/java/ve/com/abicelis/chefbuddy/app/Constants.java
@@ -9,4 +9,7 @@ public class Constants {
 
   public static final String API_KEY = "394TsayTE0ZxEgLLwuCvpfQWyoWTjGHF3UivkfZB";
 
+
+  public static final String RECIPE_LIST_INTENT_EXTRA_RECIPE_ID = "RECIPE_LIST_INTENT_EXTRA_RECIPE_ID";
+
 }
