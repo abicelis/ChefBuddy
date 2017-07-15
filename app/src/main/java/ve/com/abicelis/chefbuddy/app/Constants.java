df0eb42db25e6c49ded41822a7ce09d945592aa7
abicelis <abicelis@gmail.com>
2017-07-15 15:54:09 -0400

    Added image related constants

--- app/src/main/java/ve/com/abicelis/chefbuddy/app/Constants.java
+++ app/src/main/java/ve/com/abicelis/chefbuddy/app/Constants.java
@@ -9,7 +9,12 @@ public class Constants {
 
   public static final String API_KEY = "394TsayTE0ZxEgLLwuCvpfQWyoWTjGHF3UivkfZB";
 
+  public static final String IMAGE_FILE_EXTENSION = ".jpg";
+  public static final String IMAGE_FILES_DIR = "attachments/image";
+  public static final String IMAGE_FILENAMES_SEPARATOR = "|";
+
 
   public static final String RECIPE_LIST_INTENT_EXTRA_RECIPE_ID = "RECIPE_LIST_INTENT_EXTRA_RECIPE_ID";
+
 
 }
