f3747b0dccf6df8d2eb1be988a5fd2ff4ffd47b7
abicelis <abicelis@gmail.com>
2017-07-16 00:54:50 -0400

    Recycler, layout, adapter for recipeImages in RecipeDetailActivity

---
+++ app/src/main/java/ve/com/abicelis/chefbuddy/ui/recipeDetail/ImageAdapter.java
@@ -0,0 +1,50 @@
+package ve.com.abicelis.chefbuddy.ui.recipeDetail;
+
+import android.app.Activity;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import ve.com.abicelis.chefbuddy.R;
+import ve.com.abicelis.chefbuddy.model.Image;
+import ve.com.abicelis.chefbuddy.model.Recipe;
+
+/**
+ * Created by abicelis on 15/7/2017.
+ */
+
+public class ImageAdapter extends RecyclerView.Adapter<ImageViewHolder> {
+
+    //DATA
+    private Activity mActivity;
+    private LayoutInflater mInflater;
+    private List<Image> mImages = new ArrayList<>();
+
+    public ImageAdapter(Activity activity) {
+        mActivity = activity;
+        mInflater = LayoutInflater.from(activity);
+    }
+
+    @Override
+    public ImageViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        return new ImageViewHolder(mInflater.inflate(R.layout.list_item_image, parent, false));
+    }
+
+    @Override
+    public void onBindViewHolder(ImageViewHolder holder, int position) {
+        holder.setData(this, mActivity, mImages.get(position), position);
+        holder.setListeners();
+    }
+
+    public List<Image> getItems() {
+        return mImages;
+    }
+
+    @Override
+    public int getItemCount() {
+        return mImages.size();
+    }
+}
---
+++ app/src/main/java/ve/com/abicelis/chefbuddy/ui/recipeDetail/ImageViewHolder.java
@@ -0,0 +1,66 @@
+package ve.com.abicelis.chefbuddy.ui.recipeDetail;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import ve.com.abicelis.chefbuddy.R;
+import ve.com.abicelis.chefbuddy.app.Constants;
+import ve.com.abicelis.chefbuddy.model.Image;
+
+/**
+ * Created by abicelis on 15/7/2017.
+ */
+
+public class ImageViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+
+    //DATA
+    private ImageAdapter mAdapter;
+    private Activity mActivity;
+    private Image mCurrent;
+    private int mPosition;
+
+    //UI
+    //@BindView(R.id.list_item_image_image)
+    ImageView mImage;
+
+
+    public ImageViewHolder(View itemView) {
+        super(itemView);
+        //ButterKnife.bind(this, mActivity);
+
+        mImage = (ImageView) itemView;
+    }
+
+    public void setData(ImageAdapter adapter, Activity activity, Image current, int position) {
+        mAdapter = adapter;
+        mActivity = activity;
+        mCurrent = current;
+        mPosition = position;
+
+        mImage.setImageBitmap(mCurrent.getImage());
+    }
+
+    public void setListeners() {
+        mImage.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        int id = v.getId();
+        switch (id) {
+            case R.id.list_item_image_image:
+                Toast.makeText(v.getContext(), "Image clicked pos=" + mPosition, Toast.LENGTH_SHORT).show();
+
+                //Intent viewImageDetail = new Intent(mActivity, Blah.class);
+                //viewImageDetail.putExtra(Constants.EXTRA, mCurrent);
+                //mActivity.startActivity(viewImageDetail);
+                break;
+        }
+    }
+}
