a999c8e003e2ae4dc8cdf3999cef4884d3d39c6c
abicelis <abicelis@gmail.com>
2017-07-16 00:52:53 -0400

    Added icon drawables

--- app/src/main/java/ve/com/abicelis/chefbuddy/model/Measurement.java
+++ app/src/main/java/ve/com/abicelis/chefbuddy/model/Measurement.java
@@ -1,6 +1,10 @@
 package ve.com.abicelis.chefbuddy.model;
 
+import android.graphics.drawable.Drawable;
+import android.support.annotation.DrawableRes;
+import android.support.annotation.Nullable;
 import android.support.annotation.StringRes;
+import android.support.v4.content.ContextCompat;
 
 import ve.com.abicelis.chefbuddy.R;
 import ve.com.abicelis.chefbuddy.app.ChefBuddyApplication;
@@ -10,34 +14,36 @@ import ve.com.abicelis.chefbuddy.app.ChefBuddyApplication;
  */
 
 public enum Measurement {
-    NONE(               R.string.measurement_none,                  R.string.measurement_none),
-    INCH(               R.string.measurement_inch,              R.string.measurement_inch_abbr),
-    TABLESPOON(         R.string.measurement_tablespoon,        R.string.measurement_tablespoon_abbr),
-    TEASPOON(           R.string.measurement_teaspoon,          R.string.measurement_teaspoon_abbr),
-    CUP(                R.string.measurement_cup,               R.string.measurement_cup_abbr),
-    PINT(               R.string.measurement_pint,              R.string.measurement_pint_abbr),
-    QUART(              R.string.measurement_quart,             R.string.measurement_quart_abbr),
-    GALLON(             R.string.measurement_gallon,            R.string.measurement_gallon_abbr),
-    OUNCE(              R.string.measurement_ounce,             R.string.measurement_ounce_abbr),
-    FLUID_OUNCE(        R.string.measurement_fluid_ounce,       R.string.measurement_fluid_ounce_abbr),
+    NONE(               R.string.measurement_none,                  R.string.measurement_none, R.drawable.ic_fab_check),
+    INCH(               R.string.measurement_inch,              R.string.measurement_inch_abbr, R.drawable.ic_measurement_inch),
+    TABLESPOON(         R.string.measurement_tablespoon,        R.string.measurement_tablespoon_abbr, R.drawable.ic_measurement_tablespoon),
+    TEASPOON(           R.string.measurement_teaspoon,          R.string.measurement_teaspoon_abbr, R.drawable.ic_measurement_teaspoon),
+    CUP(                R.string.measurement_cup,               R.string.measurement_cup_abbr, R.drawable.ic_measurement_cup),
+    PINT(               R.string.measurement_pint,              R.string.measurement_pint_abbr, R.drawable.ic_measurement_pint),
+    QUART(              R.string.measurement_quart,             R.string.measurement_quart_abbr, R.drawable.ic_measurement_quart),
+    GALLON(             R.string.measurement_gallon,            R.string.measurement_gallon_abbr, R.drawable.ic_measurement_gallon),
+    OUNCE(              R.string.measurement_ounce,             R.string.measurement_ounce_abbr, R.drawable.ic_measurement_ounce),
+    FLUID_OUNCE(        R.string.measurement_fluid_ounce,       R.string.measurement_fluid_ounce_abbr, R.drawable.ic_measurement_fluid_ounce),
 
-    CENTIMETER(         R.string.measurement_centimeter,        R.string.measurement_centimeter_abbr),
-    METER(              R.string.measurement_meter,             R.string.measurement_meter_abbr),
+    CENTIMETER(         R.string.measurement_centimeter,        R.string.measurement_centimeter_abbr, R.drawable.ic_measurement_centimeter),
+    METER(              R.string.measurement_meter,             R.string.measurement_meter_abbr, R.drawable.ic_measurement_meter),
 
-    CUBIC_CENTIMITER(   R.string.measurement_cubic_centimeter,  R.string.measurement_cubic_centimeter_abbr),
-    MILILITER(          R.string.measurement_mililiter,         R.string.measurement_mililiter_abbr),
-    LITER(              R.string.measurement_liter,             R.string.measurement_liter_abbr),
+    CUBIC_CENTIMITER(   R.string.measurement_cubic_centimeter,  R.string.measurement_cubic_centimeter_abbr, R.drawable.ic_measurement_cubic_centimeter),
+    MILILITER(          R.string.measurement_mililiter,         R.string.measurement_mililiter_abbr, R.drawable.ic_measurement_mililiter),
+    LITER(              R.string.measurement_liter,             R.string.measurement_liter_abbr, R.drawable.ic_measurement_liter),
 
-    GRAM(               R.string.measurement_gram,              R.string.measurement_gram_abbr),
-    KILOGRAM(           R.string.measurement_kilogram,          R.string.measurement_kilogram_abbr),
+    GRAM(               R.string.measurement_gram,              R.string.measurement_gram_abbr, R.drawable.ic_measurement_gram),
+    KILOGRAM(           R.string.measurement_kilogram,          R.string.measurement_kilogram_abbr, R.drawable.ic_measurement_kg),
     ;
 
     private @StringRes int friendlyName;
     private @StringRes int abbreviation;
+    private @DrawableRes int icon;
 
-    Measurement(@StringRes int friendlyName,  @StringRes int abbreviation){
+    Measurement(@StringRes int friendlyName,  @StringRes int abbreviation, @DrawableRes int icon){
         this.friendlyName = friendlyName;
         this.abbreviation = abbreviation;
+        this.icon = icon;
     }
 
     public String getAbbreviation() {
@@ -46,5 +52,9 @@ public enum Measurement {
 
     public String getFriendlyName() {
         return ChefBuddyApplication.getContext().getString(friendlyName);
+    }
+
+    public @Nullable Drawable getIcon() {
+        return ContextCompat.getDrawable(ChefBuddyApplication.getContext(), icon);
     }
 }
