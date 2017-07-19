becb8538f2869a73477dec10e1e15d363c7f8abd
abicelis <abicelis@gmail.com>
2017-07-19 11:30:20 -0400

    Parcelable implementation exercise with http://www.parcelabler.com/

--- app/src/main/java/ve/com/abicelis/chefbuddy/model/Image.java
+++ app/src/main/java/ve/com/abicelis/chefbuddy/model/Image.java
@@ -1,6 +1,8 @@
 package ve.com.abicelis.chefbuddy.model;
 
 import android.graphics.Bitmap;
+import android.os.Parcel;
+import android.os.Parcelable;
 import android.support.annotation.NonNull;
 
 import ve.com.abicelis.chefbuddy.util.FileUtil;
@@ -9,8 +11,8 @@ import ve.com.abicelis.chefbuddy.util.ImageUtil;
 /**
  * Created by abicelis on 15/7/2017.
  */
-
-public class Image {
+/* Parcelable made with http://www.parcelabler.com/ */
+public class Image implements Parcelable {
 
     private String filename;
     private Bitmap image;
@@ -36,4 +38,36 @@ public class Image {
 
         return image;
     }
+
+
+
+
+    protected Image(Parcel in) {
+        filename = in.readString();
+        image = (Bitmap) in.readValue(Bitmap.class.getClassLoader());
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(filename);
+        dest.writeValue(image);
+    }
+
+    @SuppressWarnings("unused")
+    public static final Parcelable.Creator<Image> CREATOR = new Parcelable.Creator<Image>() {
+        @Override
+        public Image createFromParcel(Parcel in) {
+            return new Image(in);
+        }
+
+        @Override
+        public Image[] newArray(int size) {
+            return new Image[size];
+        }
+    };
 }
