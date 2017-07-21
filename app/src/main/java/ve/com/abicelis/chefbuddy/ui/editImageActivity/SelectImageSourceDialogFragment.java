77fa6f37c0b26d7bfaf38e14bc70f965f5aa4284
abicelis <abicelis@gmail.com>
2017-07-21 10:57:12 -0400

    SelectImageSource Dialog implementation

---
+++ app/src/main/java/ve/com/abicelis/chefbuddy/ui/editImageActivity/SelectImageSourceDialogFragment.java
@@ -0,0 +1,102 @@
+package ve.com.abicelis.chefbuddy.ui.editImageActivity;
+
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v4.app.DialogFragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import ve.com.abicelis.chefbuddy.R;
+import ve.com.abicelis.chefbuddy.model.ImageSourceType;
+
+
+/**
+ * Created by abice on 16/3/2017.
+ */
+
+public class SelectImageSourceDialogFragment extends DialogFragment implements View.OnClickListener {
+
+    //DATA
+    private ImageSourceSelectedListener mListener;
+
+    //UI
+    @BindView(R.id.dialog_select_image_source_camera)
+    FloatingActionButton mCamera;
+
+    @BindView(R.id.dialog_select_image_source_gallery)
+    FloatingActionButton mGallery;
+
+    @BindView(R.id.dialog_select_image_source_cancel)
+    Button mCancel;
+
+
+    public SelectImageSourceDialogFragment() {
+        // Empty constructor is required for DialogFragment
+        // Make sure not to add arguments to the constructor
+        // Use `newInstance` instead as shown below
+    }
+
+    public static SelectImageSourceDialogFragment newInstance() {
+        SelectImageSourceDialogFragment frag = new SelectImageSourceDialogFragment();
+        return frag;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setRetainInstance(true);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View dialogView =  inflater.inflate(R.layout.dialog_select_image_source, container);
+        ButterKnife.bind(this, dialogView);
+
+
+        mCamera.setOnClickListener(this);
+        mGallery.setOnClickListener(this);
+        mCancel.setOnClickListener(this);
+        return dialogView;
+    }
+
+    @Override
+    public void onCancel(DialogInterface dialog) {
+        super.onCancel(dialog);
+        mListener.onSourceSelected(ImageSourceType.NONE);
+    }
+
+    @Override
+    public void onClick(View v) {
+        int id = v.getId();
+        switch(id) {
+            case R.id.dialog_select_image_source_camera:
+                mListener.onSourceSelected(ImageSourceType.CAMERA);
+                break;
+
+            case R.id.dialog_select_image_source_gallery:
+                mListener.onSourceSelected(ImageSourceType.GALLERY);
+                break;
+
+            case R.id.dialog_select_image_source_cancel:
+                mListener.onSourceSelected(ImageSourceType.NONE);
+                break;
+        }
+        dismiss();
+    }
+
+
+    public void setListener(ImageSourceSelectedListener listener) {
+        mListener = listener;
+    }
+
+
+    public interface ImageSourceSelectedListener {
+        void onSourceSelected(ImageSourceType imageSourceType);
+    }
+}
