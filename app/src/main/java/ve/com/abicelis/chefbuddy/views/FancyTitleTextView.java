282e70e028cdb9ebbcf737039bbf2d15f85721be
abicelis <abicelis@gmail.com>
2017-07-23 15:26:59 -0400

    Fixed layout title margins by applying default_padding attr on custom FancyTitleTextView

--- app/src/main/java/ve/com/abicelis/chefbuddy/views/FancyTitleTextView.java
+++ app/src/main/java/ve/com/abicelis/chefbuddy/views/FancyTitleTextView.java
@@ -1,6 +1,7 @@
 package ve.com.abicelis.chefbuddy.views;
 
 import android.content.Context;
+import android.content.res.TypedArray;
 import android.support.annotation.Nullable;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.widget.AppCompatTextView;
@@ -18,21 +19,28 @@ public class FancyTitleTextView extends AppCompatTextView {
 
     public FancyTitleTextView(Context context) {
         super(context);
-        init(context);
+        init(context, null);
     }
     public FancyTitleTextView(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
-        init(context);
+        init(context, attrs);
     }
     public FancyTitleTextView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
-        init(context);
+        init(context, attrs);
     }
 
-    private void init(Context context) {
+    private void init(Context context, @Nullable AttributeSet attrs) {
+        //Get/apply custom xml configs
+        TypedArray a = context.obtainStyledAttributes(attrs, R.styleable.fancy_title_text_view);
+        boolean useDefaultPadding = a.getBoolean(R.styleable.fancy_title_text_view_default_padding, true);
+        a.recycle();
+
+
         this.setTextSize(TypedValue.COMPLEX_UNIT_SP, 26);
         this.setTextColor(ContextCompat.getColor(context, R.color.primary));
-        this.setPadding((int)DimensionUtil.convertDpToPixel(24), (int)DimensionUtil.convertDpToPixel(24), 0, 0);
+        if(useDefaultPadding)
+            this.setPadding((int)DimensionUtil.convertDpToPixel(24), (int)DimensionUtil.convertDpToPixel(24), 0, 0);
 
     }
 
