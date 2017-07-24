29b7b6e4be8479d5fa407ff5bc1e0880d3b88ec0
abicelis <abicelis@gmail.com>
2017-07-23 23:01:24 -0400

    Added lots of more measurements

--- app/src/main/java/ve/com/abicelis/chefbuddy/model/Measurement.java
+++ app/src/main/java/ve/com/abicelis/chefbuddy/model/Measurement.java
@@ -17,26 +17,35 @@ import ve.com.abicelis.chefbuddy.app.ChefBuddyApplication;
  */
 
 public enum Measurement {
-    NONE(               R.string.measurement_none,                  R.string.measurement_none, R.drawable.ic_fab_check),
-    INCH(               R.string.measurement_inch,              R.string.measurement_inch_abbr, R.drawable.ic_measurement_inch),
-    TABLESPOON(         R.string.measurement_tablespoon,        R.string.measurement_tablespoon_abbr, R.drawable.ic_measurement_tablespoon),
-    TEASPOON(           R.string.measurement_teaspoon,          R.string.measurement_teaspoon_abbr, R.drawable.ic_measurement_teaspoon),
-    CUP(                R.string.measurement_cup,               R.string.measurement_cup_abbr, R.drawable.ic_measurement_cup),
-    PINT(               R.string.measurement_pint,              R.string.measurement_pint_abbr, R.drawable.ic_measurement_pint),
-    QUART(              R.string.measurement_quart,             R.string.measurement_quart_abbr, R.drawable.ic_measurement_quart),
-    GALLON(             R.string.measurement_gallon,            R.string.measurement_gallon_abbr, R.drawable.ic_measurement_gallon),
-    OUNCE(              R.string.measurement_ounce,             R.string.measurement_ounce_abbr, R.drawable.ic_measurement_ounce),
-    FLUID_OUNCE(        R.string.measurement_fluid_ounce,       R.string.measurement_fluid_ounce_abbr, R.drawable.ic_measurement_fluid_ounce),
+    NONE(               R.string.measurement_none,              R.string.measurement_none,                      R.drawable.ic_fab_check),
+    INCH(               R.string.measurement_inch,              R.string.measurement_inch_abbr,                 R.drawable.ic_measurement_inch),
+    TABLESPOON(         R.string.measurement_tablespoon,        R.string.measurement_tablespoon_abbr,           R.drawable.ic_measurement_tablespoon),
+    TEASPOON(           R.string.measurement_teaspoon,          R.string.measurement_teaspoon_abbr,             R.drawable.ic_measurement_teaspoon),
+    CUP(                R.string.measurement_cup,               R.string.measurement_cup_abbr,                  R.drawable.ic_measurement_cup),
+    PINT(               R.string.measurement_pint,              R.string.measurement_pint_abbr,                 R.drawable.ic_measurement_pint),
+    QUART(              R.string.measurement_quart,             R.string.measurement_quart_abbr,                R.drawable.ic_measurement_quart),
+    GALLON(             R.string.measurement_gallon,            R.string.measurement_gallon_abbr,               R.drawable.ic_measurement_gallon),
+    POUND(              R.string.measurement_pound,             R.string.measurement_pound_abbr,                R.drawable.ic_measurement_pound),
+    OUNCE(              R.string.measurement_ounce,             R.string.measurement_ounce_abbr,                R.drawable.ic_measurement_ounce),
+    FLUID_OUNCE(        R.string.measurement_fluid_ounce,       R.string.measurement_fluid_ounce_abbr,          R.drawable.ic_measurement_fluid_ounce),
 
-    CENTIMETER(         R.string.measurement_centimeter,        R.string.measurement_centimeter_abbr, R.drawable.ic_measurement_centimeter),
-    METER(              R.string.measurement_meter,             R.string.measurement_meter_abbr, R.drawable.ic_measurement_meter),
+    CENTIMETER(         R.string.measurement_centimeter,        R.string.measurement_centimeter_abbr,           R.drawable.ic_measurement_centimeter),
+    METER(              R.string.measurement_meter,             R.string.measurement_meter_abbr,                R.drawable.ic_measurement_meter),
 
-    CUBIC_CENTIMITER(   R.string.measurement_cubic_centimeter,  R.string.measurement_cubic_centimeter_abbr, R.drawable.ic_measurement_cubic_centimeter),
-    MILILITER(          R.string.measurement_mililiter,         R.string.measurement_mililiter_abbr, R.drawable.ic_measurement_mililiter),
-    LITER(              R.string.measurement_liter,             R.string.measurement_liter_abbr, R.drawable.ic_measurement_liter),
+    CUBIC_CENTIMITER(   R.string.measurement_cubic_centimeter,  R.string.measurement_cubic_centimeter_abbr,     R.drawable.ic_measurement_cubic_centimeter),
+    MILILITER(          R.string.measurement_mililiter,         R.string.measurement_mililiter_abbr,            R.drawable.ic_measurement_mililiter),
+    LITER(              R.string.measurement_liter,             R.string.measurement_liter_abbr,                R.drawable.ic_measurement_liter),
 
-    GRAM(               R.string.measurement_gram,              R.string.measurement_gram_abbr, R.drawable.ic_measurement_gram),
-    KILOGRAM(           R.string.measurement_kilogram,          R.string.measurement_kilogram_abbr, R.drawable.ic_measurement_kg),
+    GRAM(               R.string.measurement_gram,              R.string.measurement_gram_abbr,                 R.drawable.ic_measurement_gram),
+    KILOGRAM(           R.string.measurement_kilogram,          R.string.measurement_kilogram_abbr,             R.drawable.ic_measurement_kg),
+
+    CAN(                R.string.measurement_can,               R.string.measurement_can_abbr,                  R.drawable.ic_measurement_can),
+    NUT(                R.string.measurement_nut,               R.string.measurement_nut_abbr,                  R.drawable.ic_measurement_nut),
+    CLOVE(              R.string.measurement_clove,             R.string.measurement_clove_abbr,                R.drawable.ic_measurement_clove),
+    JAR(                R.string.measurement_jar,               R.string.measurement_jar_abbr,                  R.drawable.ic_measurement_jar),
+    PACKAGE(            R.string.measurement_package,           R.string.measurement_package_abbr,              R.drawable.ic_measurement_box),
+    BOX(                R.string.measurement_box,               R.string.measurement_box_abbr,                  R.drawable.ic_measurement_box),
+    SHEET(              R.string.measurement_sheet,             R.string.measurement_sheet_abbr,                R.drawable.ic_measurement_sheet),
     ;
 
     private @StringRes int friendlyName;
