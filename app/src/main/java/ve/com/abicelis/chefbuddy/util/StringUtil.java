a09a258a75dd1fd46b60a8b26b57b757a84a66f3
abicelis <abicelis@gmail.com>
2017-07-25 11:44:34 -0400

    startWithUppercase fix for strings with more than one word

--- app/src/main/java/ve/com/abicelis/chefbuddy/util/StringUtil.java
+++ app/src/main/java/ve/com/abicelis/chefbuddy/util/StringUtil.java
@@ -101,4 +101,34 @@ public class StringUtil {
 
         return v0[s2.length()];
     }
+
+    public static String startWithUppercase(String string) {
+        string = string.trim();
+
+        if(string.length() == 0)
+            return string;
+
+        if(string.length() == 1)
+            return string.toUpperCase();
+
+        String[] words = string.split(" ");
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < words.length; i++) {
+            String word = words[i];
+            if(i == 0)
+                sb.append(word.substring(0, 1).toUpperCase());
+            else
+                sb.append(word.substring(0, 1));
+
+            if(word.length() > 1)
+                sb.append(word.substring(1).toLowerCase());
+
+            sb.append(" ");
+        }
+
+        if(sb.length() > 0)
+            sb.setLength(sb.length() - 1);
+
+        return sb.toString();
+    }
 }
