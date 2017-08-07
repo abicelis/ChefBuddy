620c047aa1c6d7c11238f647896140884d86b846
abicelis <abicelis@gmail.com>
2017-08-06 22:42:26 -0400

    Simplified code, added hook to BackupService v2

--- app/src/main/java/ve/com/abicelis/chefbuddy/service/BackupServiceStarter.java
+++ app/src/main/java/ve/com/abicelis/chefbuddy/service/BackupServiceStarter.java
@@ -15,37 +15,37 @@ import ve.com.abicelis.chefbuddy.util.SharedPreferenceUtil;
  */
 
 public class BackupServiceStarter extends BroadcastReceiver {
-    public static final int PENDING_INTENT_REQUEST_CODE = 300;
+    public static final int PENDING_INTENT_REQUEST_CODE = 29;
 
     @Override
     public void onReceive(Context context, Intent intent) {
-        AlarmManager alarmManager = (AlarmManager)context.getSystemService(Context.ALARM_SERVICE);
 
         //If boot completed, set alarm
         if ( intent.getAction().equals(Constants.BOOT_COMPLETED_ACTION)) {
-            setAlarmForBackup(alarmManager, context);
+            setAlarmForBackup(context);
         }
 
         //If starting app for the first time (such as when recently installed), set alarm
         if (intent.getAction().equals(Constants.APP_STARTED_ACTION) && SharedPreferenceUtil.isFirstTimeLaunchingApp()) {
-            setAlarmForBackup(alarmManager, context);
+            setAlarmForBackup(context);
         }
 
         //If backup frequency changed, set alarm
         if (intent.getAction().equals(Constants.BACKUP_FREQUENCY_CHANGED_ACTION)) {
-            setAlarmForBackup(alarmManager, context);
+            setAlarmForBackup(context);
         }
     }
 
 
-    private void setAlarmForBackup(AlarmManager alarmManager, Context context) {
+    private void setAlarmForBackup(Context context) {
+        AlarmManager alarmManager = (AlarmManager)context.getSystemService(Context.ALARM_SERVICE);
 
         //Set alarm to occur at nextAlarmTime, and repeat every intervalMillis
         long intervalMillis = SharedPreferenceUtil.getBackupFrequencyType().getMillis();
         long nextAlarmTime = SystemClock.elapsedRealtime() + intervalMillis;
 
         //Create intent and pendingIntent
-        Intent triggerBackupService = new Intent(context, BackupService.class);
+        Intent triggerBackupService = new Intent(context, BackupServiceV2.class);
         PendingIntent triggerBackupServicePendingIntent =
                 PendingIntent.getService(context, PENDING_INTENT_REQUEST_CODE, triggerBackupService, PendingIntent.FLAG_UPDATE_CURRENT);
 
